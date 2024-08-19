451287208c65e3db835e21ea7df7251d3990d9fe
RobertUlloa <roanuc8@gmail.com>
2024-08-18 22:56:48 -0500

    creation part of the public folder(css ans js)

---
+++ public/js/login.js
@@ -0,0 +1,22 @@
+const logingFormhandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector('#username-login').ariaValueMax.trim();
+    const password = document.querySelector('#password-login').ariaValueMax.trim();
+
+    if (username && password) {
+        const response = await fetch('/api/users/login',{
+            method: 'POST',
+            body: JSON.stringify({ username, password}),
+            headers: {'Content-Type': 'application/json'},
+        });
+        
+        if(response.ok) {
+            document.location.replace('/');
+        } else {
+            alert('Failed to log in.');
+        }
+    }
+};
+
+document.querySelector('.login-form').addEventListener('submit', logingFormhandler);
